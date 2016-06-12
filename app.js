752241650aeff906eb47ae297ce5621c137033b6
ibotdotout <tkroputa@gmail.com>
2016-06-12 23:32:10 +0700

    Refactoring /hi

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var hello = (req, res) => {
   res.send('hello')
 }
 
+var hi = (req, res) => {
+  res.send('hi')
+}
+
 app.get('/', hello)
 
-app.get('/hi', function (req, res) {
-  res.send('hi')
-});
+app.get('/hi', hi)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
